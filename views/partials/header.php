67e85a91365a378e0e849dc36af78ed8a2825ce3
Eli Leyder <eli.leyder@gmail.com>
2023-11-16 16:47:44 +0100

    ok

--- views/partials/header.php
+++ views/partials/header.php
@@ -13,15 +13,15 @@
         <div>
             <img src="public/img/pokemon-logo.png" alt="logoPokemon">
         </div>
+        
         <div class="navbar">
             <ul class="navbar-list">
                 <li><a href="#"><h2>Pokemon</h2></a></li>
-
                 <li><a href="#"><h2>My account</h2></a></li>
-
                 <li><a href="#"><h2>Register</h2></a></li>
             </ul>
         </div>
+
         <div>
             <input type="text" placeholder="Search..">
         </div>
