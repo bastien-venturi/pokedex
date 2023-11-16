9a402ec4f82c1ec91544071adea7def652b810e5
antoinel74 <antoine.lansman@gmail.com>
2023-11-16 15:26:06 +0100

    fix

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 // Simple Router
 
 // Include the helper file for handling requests
-require_once __DIR__.'/helpers/request.php';
+require_once __DIR__ . '/helpers/request.php';
 
 // Switch statement to handle different routes based on the path from the URL
-switch($url['path'])
-
-    // Case: Root path '/'
+switch ($url['path']) {
+        // Case: Root path '/'
     case '/':
         // Check if the HTTP method is GET
         if ($method == 'GET') {
@@ -24,7 +23,6 @@ switch($url['path'])
             // Parse the query string of the URL and store the result in the 'result' array
             parse_str($url['query'], $result);
             // Check if the 'pokemon' parameter is set in the query string
-
             if (isset($result['name']) && !empty($result['name'])) {
 
                 // If 'pokemon' parameter is set, include the 'views/show.php' file
@@ -38,38 +36,7 @@ switch($url['path'])
         }
         break;
 
-        
-    case '/userform':
-        // Check if the HTTP method is GET
-        if($method == 'GET') {
-            // Include the 'views/index.php' file for the root path
-            require 'controllers/UserController.php';
-            index();
-        }
-        else {
-            // If 'pokemon' parameter is not set, include the 'views/errors/404.php' file
-            require 'views/errors/404.php';
-            // Set HTTP response code to 404 Not Found
-            http_response_code(404);
-        }
-        break;
-
-    // Case : login
-    case '/login':
-
-        if ($method == 'GET') {
-
-            // If 'login' parameter is set, include the 'views/show.php' file
-            require 'controllers/loginController.php';
-            index();
-        } else {
-            // If 'login' parameter is not set, include the 'views/errors/404.php' file
-            require 'views/errors/404.php';
-            // Set HTTP response code to 404 Not Found
-            http_response_code(404);
-        }
-        break;
-    // Default case: Handle all other paths
+        // Default case: Handle all other paths
     default:
         // Include the 'views/errors/404.php' file for unknown paths
         require 'views/errors/404.php';
