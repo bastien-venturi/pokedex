b9be6e7b34feed910eb3e077a52ce58306284f81
antoinel74 <antoine.lansman@gmail.com>
2023-11-17 15:21:47 +0100

    add showview + showcontroller fix

--- views/show.php
+++ views/show.php
@@ -2,14 +2,50 @@
 $title = $_GET['name'];
 require_once __DIR__ . '/partials/header.php';
 require './controllers/showController.php';
-?>
+$pokemonDetails = fetchDetails();
 ?>
 
 <main>
-    <h1>
-        <?php echo $_GET['name']; ?>
-    </h1>
-    <a href="/">Homepage</a>
+    <div class="pokemon_container">
+        <?php if ($pokemonDetails) : ?>
+            <div class="pokemon_row">
+                <h1><?= $pokemonDetails['name']; ?></h1>
+                <ul>
+                    <li class="<?= $pokemonDetails['primary_type_name']; ?>"><?= $pokemonDetails['primary_type_name']; ?></li>
+                    <li class="<?= $pokemonDetails['secondary_type_name']; ?>"><?= $pokemonDetails['secondary_type_name']; ?></li>
+                </ul>
+                <div class="pokemon_stats">
+                    <p class="label">HP :</p>
+                    <p class="results"><?= $pokemonDetails['hp'] ?></p>
+                    <p class="label">Attack :</p>
+                    <p class="results"><?= $pokemonDetails['attack'] ?></p>
+                    <p class="label">Defense :</p>
+                    <p class="results"><?= $pokemonDetails['defense'] ?></p>
+                    <p class="label">Specific defense :</p>
+                    <p class="results"><?= $pokemonDetails['specific_defense'] ?></p>
+                    <p class="label">Specific attack :</p>
+                    <p class="results"><?= $pokemonDetails['specific_attack'] ?></p>
+                    <p class="label">Speed :</p>
+                    <p class="results"><?= $pokemonDetails['speed'] ?></p>
+                </div>
+                <h2>Evolutions :</h2>
+                <ul class="pokemon_evo">
+                    <li class="evo_1"><img src="/public/img/pokemon/<?= $pokemonDetails['evol_1_name']; ?>.png"></li>
+                    <li class="evo_2"><img src="/public/img/pokemon/<?= $pokemonDetails['evol_2_name']; ?>.png"></li>
+                    <li class="evo_3"><img src="/public/img/pokemon/<?= $pokemonDetails['evol_3_name']; ?>.png"></li>
+                </ul>
+            </div>
+            <div class="pokemon_row">
+                <span class="type">#0000<?= $pokemonDetails['id'];  ?></span>
+                <figure>
+                    <img src="<?= $pokemonDetails['url_img']; ?>" alt="<?= $pokemonDetails['name']; ?> image">
+                </figure>
+            </div>
+        <?php else : ?>
+            <p>Pokémon not found!</p>
+        <?php endif; ?>
+        <a href="/">Homepage</a>
+    </div>
 </main>
 
 <?php
