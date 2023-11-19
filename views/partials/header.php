134122b6d8a33c96aadcc13ec2b5a44c15df2c93
Eli Leyder <eli.leyder@gmail.com>
2023-11-19 21:55:06 +0100

    partials soonsoon xoxo forever pcq tu kiffes les commit tout ca tout ca

--- views/partials/header.php
+++ views/partials/header.php
@@ -9,36 +9,33 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100;0,300;0,600;0,900;1,100;1,300;1,600;1,900&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/ceda9163bb.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/ceda9163bb.js" crossorigin="anonymous"></script> 
 </head>
 
 <body>
     <header>
-        <div class="header-pic">
-            <img src="public/img/pokemon-logo.png" alt="logoPokemon">
-        </div>
-        
-        <nav class="navbar">
-            <div class="mask">
-                <ul class="navbar-list">
-                    <li><a href="#"><h2>Pokemon</h2></a></li>
-                    <li><a href="#"><h2>My account</h2></a></li>
-                    <li><a href="#"><h2>Register</h2></a></li>
-                </ul>
+ 
+            <div class="header-logo">
+                <img src="public/img/pokemon-logo.png" alt="logoPokemon">
             </div>
-        </nav>
-<!-- 
-        <div class="navbar-search">
-            <form action="" autocomplete="on">
-                <input class="input-search" name="search" type="text" placeholder="Search ..">
-                <input class="input-search-submit" id="search_submit" value="Rechercher" type="submit">
-            </form>
-        </div> -->
-        <div class="navbar-search">
-            <form action="" autocomplete="on">
-                <input class="input-search" name="search" type="text" placeholder="Search ..">
-                <button class="btn-search" id="search_submit" value="Rechercher" type="submit"><i class="fa fa-search"></i></button>          
-            </form>
-        </div>
+            
+            <nav class="navbar">
+                <ul class="navbar-list">
+                    <li><a href="#" class="btn-color-blue">Pokemon</a></li>
+                    <li><a href="#" class="btn-color-yellow">My account</a></li>
+                    <li><a href="#"class="btn-color-red">Register</a></li>
+                </ul>
+            </nav>
+
+            <div class="navbar-search">
+                <h2>Find your Pokemon</h2>
+                <div class="form-container">
+                    <form action="" autocomplete="on">
+                        <input class="input-search" name="search" type="text" placeholder="Search ..">
+                        <button class="btn-search" id="search_submit" value="Rechercher" type="submit"><i class="fa fa-search"></i></button>          
+                    </form>
+                </div>
+            </div>
 
     </header>
