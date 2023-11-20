4635a3c814b6c5430acf24646a633c72578d2e2e
antoinel74 <antoine.lansman@gmail.com>
2023-11-20 12:20:26 +0100

    style fav

--- views/fav.views.php
+++ views/fav.views.php
@@ -10,45 +10,34 @@ require_once __DIR__ . '/partials/header.php';
 
 ?>
 
-    <main>
-        <h2>Mes favoris</h2>
-        <table>
+<main>
+    <h2 class="view_title">Mes favoris</h2>
+    <table>
+        <?php foreach ($data as $row) : ?>
             <tr>
-                <th></th>
-                <th>Pokemon</th>
-                <th>Stats</th>
-                <th>Delete</th>
+                <td><img src="<?php echo $row['url_img'] ?>" alt="pokemon_img" class="fav_pkmn"></td>
+                <td><?php echo $row['name'] ?></td>
+                <td>
+                    <ul>
+                        <li>hp : <?php echo $row['hp'] ?></li>
+                        <li>attack : <?php echo $row['attack'] ?></li>
+                        <li>defense : <?php echo $row['defense'] ?></li>
+                        <li>specific_defense : <?php echo $row['specific_defense'] ?></li>
+                        <li>specific_attack : <?php echo $row['specific_attack'] ?></li>
+                        <li>speed : <?php echo $row['speed'] ?></li>
+                    </ul>
+                </td>
+                <td>
+                    <form action="../controllers/delFav.php" method="post">
+                        <input type="hidden" name="pokeId" value="<?php echo $row['pokemon_id'] ?>">
+                        <input type="submit" value="delete">
+                    </form>
+                </td>
             </tr>
-               
-            
-                <?php foreach ($data as $row) : ?>
-                <tr>
-                    <td><img src="<?php echo $row['url_img'] ?>" alt="pokemon_img"></td>
-                    <td><?php echo $row['name'] ?></td>
-                    <td>
-                        <ul>
-                            <li>hp : <?php echo $row['hp'] ?></li>
-                            <li>attack : <?php echo $row['attack'] ?></li>
-                            <li>defense : <?php echo $row['defense'] ?></li>
-                            <li>specific_defense : <?php echo $row['specific_defense'] ?></li>
-                            <li>specific_attack : <?php echo $row['specific_attack'] ?></li>
-                            <li>speed : <?php echo $row['speed'] ?></li>
-                        </ul>
-                    </td>
-                    <td>
-                        <form action="../controllers/delFav.php" method="post">
-                            <input type="hidden" name="pokeId" value="<?php echo $row['pokemon_id'] ?>">
-                            <input type="submit" value="delete">
-                        </form>
-                    </td>
-                    
-                    <?php endforeach;?>
-
-                
-            
-        </table>
-    </main>
+        <?php endforeach; ?>
+    </table>
+</main>
 
 <?php
 
-require_once __DIR__.'/partials/footer.php' ;
+require_once __DIR__ . '/partials/footer.php';
