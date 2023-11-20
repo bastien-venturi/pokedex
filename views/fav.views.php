2be61e1fd28cc8cf1e916304ad39ba853c54b50f
antoinel74 <antoine.lansman@gmail.com>
2023-11-20 14:28:18 +0100

    fix

--- views/fav.views.php
+++ views/fav.views.php
@@ -14,11 +14,11 @@ require_once __DIR__ . '/partials/header.php';
     <h2 class="view_title">Mes favoris</h2>
     <div class="fav_container">
         <?php foreach ($data as $row) : ?>
-            <a href="pokemon?name=<?= $row['name'] ?>">
+            <a href="pokemon?name=<?= $row['poke_name'] ?>">
                 <div class="card">
                     <figure><img src="<?= $row['url_img'] ?>" alt="pokemon_img"></figure>
                     <span class="type">#0000<?= $row['pokemon_id'] ?></span>
-                    <h3><?= $row['name'] ?></h3>
+                    <h3><?= $row['poke_name'] ?></h3>
                     <?php if (isset($_SESSION['user'])) : ?>
                         <form action="../controllers/delFav.php" method="post">
                             <input type="hidden" name="pokeId" value="<?php echo $row['pokemon_id'] ?>">
